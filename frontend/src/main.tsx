219211dce9141e6ce0c65fa9f72ab3b09470ead5
xiaoqianran <1416220530@qq.com>
2026-09-08 06:21:05 +0800

    Load Gallery workspace styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
 import "./styles.css";
+import "./gallery.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
