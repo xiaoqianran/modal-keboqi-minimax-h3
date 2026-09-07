66f945579734cd6752eedb47a171c77b1f3d367d
xiaoqianran <1416220530@qq.com>
2026-09-08 06:24:07 +0800

    Load Music 3 workspace styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./styles.css";
 import "./gallery.css";
+import "./music.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
